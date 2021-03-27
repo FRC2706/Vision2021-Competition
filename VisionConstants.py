After practice in RL's basement, working well, fix to masking to capture 0 hue value

VisionConstants.py
@@ -99,17 +99,24 @@ upper_yellow = np.array([30, 255, 255])
 #lower_orange = np.array([0, 140, 180])
 #upper_orange = np.array([12, 255, 255])
 # Brian picture 
-lower_orange = np.array([0, 200, 150])
-upper_orange = np.array([3, 255, 255])
+#lower_orange = np.array([0, 200, 150])
+#upper_orange = np.array([3, 255, 255])
 # Robert experiment night 2  dining room
 #lower_orange = np.array([0, 200, 175])
 #upper_orange = np.array([3, 255, 255])
 # Wei basement 6:20pm (no lights)
-#lower_orange = np.array([0, 125, 160])
-#upper_orange = np.array([6, 190, 255])
+lower_orange = np.array([0, 125, 160])
+upper_orange = np.array([6, 190, 255])
 # Wei basement 6:40pm (lights on)
 #lower_orange = np.array([8, 100, 150])
 #upper_orange = np.array([17, 150, 200])
+# 2021-irahConeTesting
+#lower_orange = np.array([0, 120, 170])
+#upper_orange = np.array([19, 200, 255])
+# 2021-03-27 rl basement
+#lower_orange = np.array([0, 230, 160])
+#upper_orange = np.array([10, 255, 255])
+
 
 blingColour = 0
 